70beca2777faa244d59389e6cce42b141dbd23d8
Aditya Tawde <adityatawde9699@gmail.com>
2025-07-13 23:34:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,5 +96,8 @@ def admin_login():
 def get_messages():
     return jsonify(messages)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000) 
+if __name__ == "__main__":
+    
+    CORS(app)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
